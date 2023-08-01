008ade35596a09aeedbab08abe5ad59f66e0cb74
khaled <elkotkhaled802@gmail.com>
2023-08-02 01:34:28 +0300

    add index (GinIndex) to postgres

--- natiga/degree/models.py
+++ natiga/degree/models.py
@@ -1,20 +1,26 @@
 from django.db import models
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchHeadline
-from django.contrib.postgres.search import TrigramWordDistance
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchVectorField, SearchRank
+from django.contrib.postgres.indexes import GinIndex
+
 # Create your models here.
 class Degree(models.Model):
     seating_number = models.IntegerField(unique=True, db_index=True)
     name = models.CharField(max_length=256)
     student_case = models.SmallIntegerField()
     total_degree = models.FloatField(default=410)
+    vector_column = SearchVectorField(null=True)
 
     def __str__(self) -> str:
         return self.name
+    
+    class Meta:
+        indexes = (GinIndex(fields=["vector_column"]),) # add index
     
     @classmethod
     def get_by_name(cls, name):
         vector = SearchVector("name", config="arabic")
         query = SearchQuery(name, config='arabic', search_type='phrase')
+
         return cls.objects.annotate(
           search = vector
         ).filter(search=query)
